552990a3ba1ea12047bd1d5f6f702e1e7ab284b0
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-08-03 23:21:33 -0400

    Time: 2 ms (54.59%), Space: 51.9 MB (78.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] l = new int[n];
-        int[] r = new int[n];
-        
-        l[0]=nums[0];
-        r[n-1]=nums[n-1];
-        
-        r[n-1]= nums[n-1];
-        for(int i=1;i<n;i++){
-            l[i]=l[i-1]*nums[i];
-            
-        }
-        for(int i=n-2;i>=0;i--){
-            r[i]=r[i+1]*nums[i];
-            
-        }
         int[] ans = new int[n];
-        for(int i=1;i<n-1;i++){
-            ans[i]= l[i-1]*r[i+1];
+        
+        ans[0]=1;
+        ans[n-1]=1;
+        
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*prev;
+            prev= nums[i];
+            
         }
-        ans[0]=r[1];
-        ans[n-1]=l[n-2];
+        // System.out.println(Arrays.toString(ans));
+        // System.out.println(prev);
+        
+        for(int i=n-2;i>=0;i--){
+            ans[i]=ans[i]*prev;
+            prev*=nums[i];
+            
+        }
+        
+        // ans[0]=r[1];
+        // ans[n-1]=l[n-2];
         return ans;
         
     }
