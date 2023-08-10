b1a687a5e2240bd9f97ef32ce4e9d862b8ffced2
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-08-10 01:47:03 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (59.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,26 +3,49 @@ class Solution {
         int n =nums.length;
         
         // s ....nums[t]? ...m... nums[t]? ...e
-        //7 6 1 2 
         int s= 0, e= n-1;
-        while(s<=e){
-            int m= s+(e-s)/2;
-            if(nums[m]==target) return m;
-            if(nums[m]>= nums[s]){
-                if(nums[m]>target && target>=nums[s]){
-                    e=m-1;
-                }else{
-                    s=m+1;
-                }
-            }else if(nums[m]<=nums[e]){
-                if(nums[m]<target && target<=nums[e]){
-                    s=m+1;
-                }else{
-                    e=m-1;
-                }
+//         while(s<=e){
+//             int m= s+(e-s)/2;
+//             if(nums[m]==target) return m;
+//             if(nums[m]>= nums[s]){
+//                 if(nums[m]>target && target>=nums[s]){
+//                     e=m-1;
+//                 }else{
+//                     s=m+1;
+//                 }
+//             }else if(nums[m]<=nums[e]){
+//                 if(nums[m]<target && target<=nums[e]){
+//                     s=m+1;
+//                 }else{
+//                     e=m-1;
+//                 }
                 
+//             }
+//         }
+//         return -1;
+        
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m]==target){
+                return m;
             }
+                if(nums[s] <= nums[m] ){
+                    if(target< nums[m] && nums[s]<=target){
+                        e=m-1;   
+                    }else{
+                        s=m+1;    
+                    }
+                    
+                }
+                    else if(nums[e]>=nums[m]){
+                        if(target>nums[m] && target<=nums[e]){
+                            s=m+1;
+                        }else{
+                            e=m-1;
+                        }
+                    }
+                }
+            return -1;    
         }
-        return -1;
+        
     }
-}
