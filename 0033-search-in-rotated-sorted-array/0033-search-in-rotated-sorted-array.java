5946fda4ca0800f1c912740193a5726cd69e37ff
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-08-10 01:47:35 -0400

    Time: 0 ms (100.00%), Space: 40.7 MB (89.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,48 +4,26 @@ class Solution {
         
         // s ....nums[t]? ...m... nums[t]? ...e
         int s= 0, e= n-1;
-//         while(s<=e){
-//             int m= s+(e-s)/2;
-//             if(nums[m]==target) return m;
-//             if(nums[m]>= nums[s]){
-//                 if(nums[m]>target && target>=nums[s]){
-//                     e=m-1;
-//                 }else{
-//                     s=m+1;
-//                 }
-//             }else if(nums[m]<=nums[e]){
-//                 if(nums[m]<target && target<=nums[e]){
-//                     s=m+1;
-//                 }else{
-//                     e=m-1;
-//                 }
-                
-//             }
-//         }
-//         return -1;
-        
         while(s<=e){
-            int m = s+(e-s)/2;
-            if(nums[m]==target){
-                return m;
+            int m= s+(e-s)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]>= nums[s]){
+                if(nums[m]>target && target>=nums[s]){
+                    e=m-1;
+                }else{
+                    s=m+1;
+                }
+            }else if(nums[m]<=nums[e]){
+                if(nums[m]<target && target<=nums[e]){
+                    s=m+1;
+                }else{
+                    e=m-1;
+                }
+                
             }
-                if(nums[s] <= nums[m] ){
-                    if(target< nums[m] && nums[s]<=target){
-                        e=m-1;   
-                    }else{
-                        s=m+1;    
-                    }
-                    
-                }
-                    else if(nums[e]>=nums[m]){
-                        if(target>nums[m] && target<=nums[e]){
-                            s=m+1;
-                        }else{
-                            e=m-1;
-                        }
-                    }
-                }
-            return -1;    
+        }
+        return -1;
+    
         }
         
     }
