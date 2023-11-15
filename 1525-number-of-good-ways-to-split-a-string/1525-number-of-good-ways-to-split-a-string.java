db1fbf24402a7e57691aa1b2b0ad6b4177178e1c
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-14 19:50:22 -0500

    Time: 5 ms (94.29%), Space: 43.8 MB (52.86%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
@@ -1,35 +1,22 @@
 class Solution {
     public int numSplits(String s) {
         int n = s.length();
-        int[][] arr = new int[n][26];
-        arr[0][s.charAt(0)-'a']++;
-        for(int i=1;i<n;i++){
-            char ch = s.charAt(i);
-            arr[i][ch-'a']++;
-            for(int j=0;j<26;j++){
-                arr[i][j]+=arr[i-1][j];
-            }
+        int[] l = new int[26];
+        int[] r = new int[26];
+        int rightUnique=0, leftUnique=0, ans=0;
+        for(char ch : s.toCharArray()){
+            r[ch-'a']++;
+            if(r[ch-'a']==1) rightUnique++;
         }
-        int c=0;
-        for(int i=0;i<n-1;i++){
-            int c1=0,c2=0;
-            int[] a1= arr[i];
-            for(int j=0;j<26;j++){
-                if(a1[j]>0){
-                    c1++;
-                }
-            }
-            for(int j=0;j<26;j++){
-                if(arr[n-1][j]-arr[i][j]>0){
-                    c2++;
-                }
-            }
-            if(c1==c2){
-                c++;
-            }
-            // System.out.println(i +" "+c1+" "+c2);
+        for(char ch : s.toCharArray()){
+            l[ch-'a']++;
+            if(l[ch-'a']==1) leftUnique++;
+            
+            r[ch-'a']--;
+            if(r[ch-'a']==0) rightUnique--;
+            if(leftUnique==rightUnique)ans++;
             
         }
-        return c;
+        return ans;
     }
 }
