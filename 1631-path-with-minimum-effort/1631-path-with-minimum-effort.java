7cdb02fbbaab0282c4917e2983b870db8d14e9c9
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-18 03:11:54 -0500

    Time: 269 ms (5.37%), Space: 45.6 MB (5.05%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -4,7 +4,7 @@ class Solution {
     public int minimumEffortPath(int[][] heights) {
         n= heights.length;
         m= heights[0].length;
-        PriorityQueue<Pair> q= new PriorityQueue<>((a,b)-> a.d-b.d);
+        Queue<Pair> q= new LinkedList<>();
         int[][] dis = new int[n][m];
         for(int[] x:dis) Arrays.fill(x,(int)(1e9));
         dis[0][0]=0;
@@ -33,3 +33,6 @@ class Solution {
         }
     }
 }
+
+
+
