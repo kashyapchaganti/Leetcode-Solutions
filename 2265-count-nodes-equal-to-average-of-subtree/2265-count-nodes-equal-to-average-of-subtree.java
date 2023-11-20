7d32928f32e530b780f7b7cd9ea124fefa96e627
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-19 22:25:48 -0500

    Time: 1 ms (42.22%), Space: 43.7 MB (5.19%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -14,28 +14,21 @@
  * }
  */
 class Solution {
-    int count;
+    int count=0;
     public int averageOfSubtree(TreeNode root) {
-        if(root==null) return 0;
         check(root);
-        
         return count;
     }
     public Pair<Integer,Integer> check(TreeNode root){
         if(root==null){
             return new Pair(0,0);
         }
+        Pair<Integer,Integer> left = check(root.left);
+        Pair<Integer,Integer> right = check(root.right);
         
-        Pair<Integer,Integer> left= check(root.left);
-        Pair<Integer,Integer> right= check(root.right);
-        
-        
-        int nodeSum = left.getKey() + right.getKey()+root.val;
-        int nodes = left.getValue() + right.getValue()+1;
-        
-        if(nodeSum/nodes == root.val)count++;
-        
-        
-        return new Pair(nodeSum, nodes);
+        int totalSum = left.getKey()+right.getKey()+ root.val;
+        int nodes = left.getValue()+right.getValue()+1;
+        if(totalSum/nodes == root.val) count++;
+        return new Pair(totalSum, nodes);
     }
 }
