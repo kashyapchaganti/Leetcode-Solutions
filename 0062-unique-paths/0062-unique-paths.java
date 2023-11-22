afe04f691a374c30663cad2a17ab1ccdc85dcf2c
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-22 15:39:10 -0500

    Time: 0 ms (100.00%), Space: 39.6 MB (21.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,11 @@
 class Solution {
-    int m,n;
     public int uniquePaths(int m, int n) {
-        this.m=m;
-        this.n=n;
-        int[][] dp = new int[m+1][n+1];
-        for(int[] x: dp)Arrays.fill(x,-1);
-        return check(dp,0,0);
-    }
-    public int check(int[][] dp, int i, int j){
-        if( i==m-1 && j==n-1){
-            return 1;
+        int N = m+n-2;
+        int r= m-1;
+        long ans=1;
+        for(int i=1;i<=r;i++){
+            ans=ans*(N-r+i)/i; 
         }
-        if(i>=m || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int a = check(dp,i+1,j);
-        int b = check(dp,i,j+1);
-        
-        return dp[i][j]= a+b;
+        return (int)ans;
     }
 }
