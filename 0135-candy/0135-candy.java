3cb57eaf1893b9dc54eff9a98eeeb487022e6a04
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-25 12:27:21 -0500

    Time: 3 ms (82.07%), Space: 44 MB (33.10%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,19 +1,23 @@
-public class Solution {
+class Solution {
     public int candy(int[] ratings) {
-        int[] candies = new int[ratings.length];
-        Arrays.fill(candies, 1);
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        int n = ratings.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i-1]<ratings[i]){
+                ans[i]= ans[i-1]+1;
             }
         }
-        int sum = candies[ratings.length - 1];
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        // System.out.println(Arrays.toString(ans));
+        int sum=ans[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i+1]<ratings[i]){
+                ans[i]= Math.max(1+ans[i+1],ans[i]);
             }
-            sum += candies[i];
+            sum+=ans[i];
         }
+        // System.out.println(Arrays.toString(ans));
         return sum;
     }
 }
