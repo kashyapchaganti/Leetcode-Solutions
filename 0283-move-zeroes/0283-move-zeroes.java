65430ea8e648d56220f9254d2f77a5a4bf41e19e
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-25 20:21:16 -0500

    Time: 2 ms (68.27%), Space: 45.5 MB (9.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,31 +1,21 @@
 class Solution {
     public void moveZeroes(int[] arr) {
         int n = arr.length;
-    int i=0, j=0;
+        int z=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                z++;
+            }else{
+                int v= arr[i];
+                arr[i]=0;
+                arr[i-z]=v;
+                // arr[i]=0;
+                
+            }
+        }
+        return ;
 
-    while(i<n && j<n){
-      if(arr[i]==0){   
-        j= findNonZero(arr,j, n);
-        if(j==n) break;
-        swap(i,j,arr);
-        i++;
-        j++;
-      }else{
-        while(i<n && arr[i]!=0){ 
-              i++;
-          } 
-         j= findNonZero(arr,i+1, n);
-      }
-
-    }
-    return ;
-    }
-      int findNonZero(int[] arr, int j, int n){
-
-    while(j<n && arr[j]==0){
-              j++;
-     }
-    return j;
+    
   }
    void swap(int i, int j, int[] arr){
     int temp = arr[i];
@@ -33,3 +23,5 @@ class Solution {
     arr[j]=temp;
   }
 }
+
+// [1,2,0,3]
