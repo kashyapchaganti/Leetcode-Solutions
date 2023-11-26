7248f79350c7b0b62f04fec53cbfbf026b0060ce
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-25 20:33:38 -0500

    Time: 3 ms (82.19%), Space: 44.1 MB (22.96%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,25 +1,23 @@
 class Solution {
     public int candy(int[] ratings) {
-int[] res = new int[ratings.length];
-  Arrays.fill(res, 1);
-  int count = 0;
-  
-	//left pass
-  for(int i = 1; i < ratings.length; i++){
-  	if(ratings[i] > ratings[i - 1]){
-    	res[i] = res[i - 1] + 1;
-    }
-  }
-  
-  count = res[ratings.length - 1];
-  //right pass
-  for(int i = ratings.length - 2; i >= 0; i--){
-  	if(ratings[i + 1] < ratings[i] && res[i] <= res[i + 1]){
-    	res[i] = res[i + 1] + 1;
-    }
-    count += res[i];
-  }
-  
-  return count;
+        int n = ratings.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i-1]<ratings[i]){
+                ans[i]= ans[i-1]+1;
+            }
+        }
+        
+        int sum=ans[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i+1]<ratings[i]){
+                ans[i]= Math.max(1+ans[i+1],ans[i]);
+            }
+            sum+=ans[i];
+        }
+        
+        return sum;
     }
 }
