63aaf712862c2a2c865f14251f9069f8036931e3
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-28 17:23:02 -0500

    Time: 0 ms (100.00%), Space: 39.8 MB (98.92%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,30 +1,12 @@
 class Solution {
     public boolean checkValidString(String s) {
-        Boolean[][][] dp = new Boolean[101][101][101];
-        return check(dp,0,0,0,s);
-    }
-    public boolean check(Boolean[][][] dp , int i, int l, int r, String s){
-        if(i==s.length()){
-            if(l==r){
-                return true;
-            }
-            return false;
-        }
-        if(l<r) return false;
-        if(dp[i][l][r]!=null){
-            return dp[i][l][r];
-        }
-        boolean a =false;
-        if(s.charAt(i)=='('){
-            
-            a= check(dp, i+1, l+1, r,s);
-        }else if(s.charAt(i)==')'){
-            a= check(dp, i+1, l, r+1,s);
-        }else{
-            a= check(dp, i+1, l+1, r,s);
-            a = a || check(dp, i+1, l, r+1,s);
-            a= a || check(dp, i+1, l+1, r+1,s);
-        }
-        return dp[i][l][r]=a;
+       int lo = 0, hi = 0;
+       for (char c: s.toCharArray()) {
+           lo += c == '(' ? 1 : -1;
+           hi += c != ')' ? 1 : -1;
+           if (hi < 0) break;
+           lo = Math.max(lo, 0);
+       }
+       return lo == 0;
     }
 }
