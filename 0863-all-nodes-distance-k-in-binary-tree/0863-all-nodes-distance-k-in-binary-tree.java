62d06cccc1b2a1c49210b0feaf589135ee69e09b
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-29 02:15:27 -0500

    Time: 11 ms (84.06%), Space: 41.7 MB (53.94%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -9,42 +9,47 @@
  */
 
 class Solution {
-    List<Integer> finalOutput ;
-    HashMap<Integer,HashSet<Integer>> adjacencyList; 
-
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        finalOutput= new ArrayList<>(); 
-         adjacencyList= new HashMap<>();
-	buildGraph(root,-1); 
-	calculateDistanceFromSource(target.val, 0, k, -1);
-	return finalOutput;
-    }
-    public void buildGraph(TreeNode root, int parent){
- if(root==null){
-	return ; 
-}
-adjacencyList.put(root.val, adjacencyList.getOrDefault(root.val, new HashSet<>()));
-if(parent!=-1) adjacencyList.get(root.val).add(parent);
-if(root.left!=null)
-adjacencyList.get(root.val).add(root.left.val);
-if(root.right!=null)
-adjacencyList.get(root.val).add(root.right.val);
-
-buildGraph(root.left, root.val);
-buildGraph(root.right, root.val);
-
-    }
-
-public void calculateDistanceFromSource(int source, int distance, int requiredDistance, int parent){
-	if(distance == requiredDistance) finalOutput.add(source);
-	for(int child: adjacencyList.get(source)){
-		if(child == parent) continue;
-		calculateDistanceFromSource(child, distance+1, requiredDistance, source);
-
-	}
-	
-
-}
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k){
+		if(root==null) return new ArrayList<>();
+    HashMap<Integer,List<Integer>> adjacencyList = new HashMap<>(); 
+    buildGraph(root, null,adjacencyList);
+    List<Integer> result = new ArrayList<>();
+    if(!adjacencyList.containsKey(target.val)) return new ArrayList<>();
     
+    dfsTraversal(target.val,-1, 0, k, result,adjacencyList);
+    return result;
+}
+public void dfsTraversal(int src, int p, int dis, int k, List<Integer> result, HashMap<Integer,List<Integer>> adjacencyList){ // o(n)
+
+	if(dis==k){
+  		result.add(src);
+      return;
+  }
+    for(int child: adjacencyList.get(src)){
+    		if(child!=p){
+        		dfsTraversal(child, src, dis+1, k, result,adjacencyList);
+        }
+    }
+}
+public void buildGraph(TreeNode root, TreeNode parent, HashMap<Integer,List<Integer>> adjacencyList){ // o(n) n -> nodes  
+		if(root==null) return ;
+    
+    // Adding the node to the adj list
+    adjacencyList.put(root.val, new ArrayList<>());
+    
+    if(parent!=null)
+    adjacencyList.get(root.val).add(parent.val);
+    
+    if(root.left!=null){
+    	adjacencyList.get(root.val).add(root.left.val);
+    }
+    
+    if(root.right!=null){
+    	adjacencyList.get(root.val).add(root.right.val); 
+    }
+    buildGraph(root.left, root, adjacencyList);
+    buildGraph(root.right, root, adjacencyList);
+    
+}
 }
 
