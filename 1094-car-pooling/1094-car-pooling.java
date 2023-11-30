f1912e72b52ec5411d1ce1fa421d56124fcc97b4
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-11-30 02:29:15 -0500

    Time: 7 ms (34.28%), Space: 43.4 MB (28.99%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,24 +1,18 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int n = trips.length;
-        Arrays.sort(trips, (a,b)-> a[1]==b[1]? a[2]-b[2]: a[1]-b[1]);
-        int start = trips[0][1] , end = trips[0][2], c= trips[0][0];
-        if(c>capacity)return false;
-        // for(int[] x: trips){
-        //     System.out.println(Arrays.toString(x));
-        // }
+        TreeMap<Integer,Integer> m = new TreeMap<>();
+        for(int[] t: trips){
+            int start = t[1];
+            int end = t[2];
+            m.put(start, m.getOrDefault(start,0)+t[0]);
+            m.put(end, m.getOrDefault(end,0)-t[0]);
+        }
+        int total=0;
+        for(int v: m.values()){
+            total+=v;
+            if(total>capacity)return false;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[2]-b[2]);
-        pq.add(new int[]{c,start,end});
-        for(int i=1;i<n;i++){
-            while(!pq.isEmpty() && pq.peek()[2]<=trips[i][1]){
-                int[] arr=pq.poll();
-                c-=arr[0];
-            }
-            if(c+trips[i][0]>capacity)return false;
-            
-            c+=trips[i][0];
-            pq.add(trips[i]);
         }
         return true;
         
