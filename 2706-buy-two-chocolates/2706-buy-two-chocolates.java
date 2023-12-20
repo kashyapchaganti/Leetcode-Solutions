cca9f5b04cf1efd9be1b67620de221249185c9c9
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2023-12-20 02:26:14 -0500

    Time: 1 ms (100.00%), Space: 43 MB (85.01%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,10 +1,16 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         int n =prices.length;
-        Arrays.sort(prices);
-        int a= prices[0];
-        int b= prices[1];
-        if(b+a> money) return money;
-        return (money- (b+a));
+        int a=(int)(1e9), b =(int)(1e9);
+        for(int x: prices){
+            if(a>x){
+                b=a;
+                a=x;
+            }else if(b>x){
+                b=x;
+            }
+        }
+        if(b+a>money)return money;
+        return money-(b+a);
     }
 }
