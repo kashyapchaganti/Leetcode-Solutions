507b18a084da4c0c40d20aa95e32cb3153aa0a21
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2024-01-03 01:40:45 -0500

    Time: 11 ms (92.06%), Space: 44.8 MB (34.52%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,30 +1,17 @@
 class Solution {
     public int numberOfBeams(String[] nums) {
-        int n = nums.length;
-        int[]psum = new int[n];
+        int n = nums.length, prev=0,ans=0;
         
-            for(int i=0;i<nums[0].length();i++){
-                if(nums[0].charAt(i)=='1'){
-                    psum[0]++;
-                }
-            }
-        for(int i=1;i<n;i++){
-            psum[i]=psum[i-1];
+        for(int i=0;i<n;i++){
+            int count=0;
             for(int j=0;j<nums[i].length();j++){
                 if(nums[i].charAt(j)=='1'){
-                    psum[i]++;
+                    count++;
                 }
             }
-        }
-        int ans=0;
-        // System.out.println(Arrays.toString(psum));
-        for(int i=0;i<n;i++){
-            int v= i-1>=0?psum[i]-psum[i-1]: psum[i];
-            for(int j=i+1;j<n;j++){
-                if(psum[j]-psum[j-1]>0){
-                    ans+=v*(psum[j]-psum[j-1]);
-                    break;
-                }
+            if(count>0){
+                ans+=prev*count;
+                prev=count;
             }
         }
         return ans;
