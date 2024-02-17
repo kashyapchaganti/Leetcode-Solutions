adf0ee5567b22a8e112e153eb624e4c1dd3832d9
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2024-02-17 11:49:51 -0500

    Time: 52 ms (61.22%), Space: 49.3 MB (7.43%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val1) {
+    return {
+        toBe: (val2) =>{
+            if(val2===val1) return true;
+            throw new Error("Not Equal");
+            
+        },
+        notToBe: (val2) =>{
+            if(val1===val2) throw new Error("Equal");
+            return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
