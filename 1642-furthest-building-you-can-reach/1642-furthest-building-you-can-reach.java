cc970e117060bb424e63811b0b1ffb94ebe5b33a
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2024-02-17 11:18:58 -0500

    Time: 65 ms (5.56%), Space: 56.7 MB (94.71%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,21 +1,18 @@
 class Solution {
-    int n ;
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        n = heights.length;
-        int s= 0, e= n-1;
-        List<int[]> sortedClimbs = new ArrayList<>();
+    public int furthestBuilding(int[] nums, int b, int l) {
+        int n = nums.length;
+        List<Pair> arr = new ArrayList<>();
         for(int i=0;i<n-1;i++){
-            int climb= heights[i+1]-heights[i];
-            if(climb>0){
-                sortedClimbs.add(new int[]{climb, i+1});
+            if(nums[i]<nums[i+1]){
+                arr.add(new Pair(nums[i+1]-nums[i],i+1));
             }
         }
-        Collections.sort(sortedClimbs, (a,b)-> a[0]-b[0]);
-        int ans=0;
+        Collections.sort(arr, (a,b1)-> a.r==b1.r? a.c-b1.c: a.r-b1.r);
+        int s = 0, e= n-1, ans=0;
         while(s<=e){
             int m = s+(e-s)/2;
-            if(check(sortedClimbs, bricks, ladders,m)){
-                ans=m;
+            if(check(m,l,b,arr)){
+                ans = m;
                 s=m+1;
             }else{
                 e=m-1;
@@ -23,19 +20,39 @@ class Solution {
         }
         return ans;
     }
-    public boolean check(List<int[]> h, int b, int l, int m){
-        int s=0;
-        for(int[] a: h){
-            if(a[1]>m) continue;
-            if(a[0]<=b){
-                b-=a[0];
-            }else if(l>0){
-                l--;
+    public boolean check(int m, int l, int b,List<Pair> arr ){
+        for(Pair x: arr){
+            if(x.c<=m){
+                if(b>=x.r){
+                    b-=x.r;
+                }else{
+                    if(l>0){
+                        l--;
+                    }else{
+                        return false;
+                    }
+                }
             }else{
-                return false;
+                continue;
             }
         }
         return true;
     }
 }
+class Pair{
+    int r,c;
+    public Pair(int r, int c){
+        this.r=r;
+        this.c=c;
+    }
+}
 
+/*
+4,1,1,1,99999
+
+1, 1, 1 ,1 , 4 , x 
+
+4
+1
+
+*/
