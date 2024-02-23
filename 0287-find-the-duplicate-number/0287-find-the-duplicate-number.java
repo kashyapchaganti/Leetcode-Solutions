fe4760e928cb252f1eeb8deec0286a4a53f9b7c8
Kashyap Chaganti <66533610+kashyapchaganti@users.noreply.github.com>
2024-02-23 10:12:10 -0500

    Time: 5 ms (71.26%), Space: 58.4 MB (42.50%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,10 +2,10 @@ class Solution {
     public int findDuplicate(int[] nums) {
         // idea is brilliant check description
         int a = nums[0], b= nums[0];
-        do{
-            a=nums[a];
-            b=nums[nums[b]];
-        }while(a!=b);
+        a= nums[a];b=nums[nums[b]];
+        while(a!=b){
+            a= nums[a];b=nums[nums[b]];
+        }
         a= nums[0];
         while(a!=b){
             a= nums[a];
@@ -14,3 +14,12 @@ class Solution {
         return a;
     }
 }
+
+/*
+
+1 3 4 2 2 
+
+1 -> 3 - > 2  -> 4 -> 2 - > 4
+
+
+*/
